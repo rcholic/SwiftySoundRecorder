658bdac8d75ff000456148e17119fbd2b29080ca
rcholic <ivytony@gmail.com>
2016-08-15 22:10:56 -0400

    more image icons from google material

---
+++ Example/Pods/Target Support Files/FDWaveformViewForked/FDWaveformViewForked-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_FDWaveformViewForked : NSObject
+@end
+@implementation PodsDummy_FDWaveformViewForked
+@end
---
+++ Example/Pods/Target Support Files/FDWaveformViewForked/FDWaveformViewForked-umbrella.h
@@ -0,0 +1,7 @@
+#import <UIKit/UIKit.h>
+
+#import "FDWaveformView.h"
+
+FOUNDATION_EXPORT double FDWaveformViewForkedVersionNumber;
+FOUNDATION_EXPORT const unsigned char FDWaveformViewForkedVersionString[];
+
